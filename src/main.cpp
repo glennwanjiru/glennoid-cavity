a29802f166be2030e98d03120fbc8a434cb2de73
glennwanjiru <glennnjoroge@gmail.com>
2024-02-13 11:17:46 +0300

    he

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,35 @@
-#include <Arduino.h>
+#include <DHT.h>
+#include <LiquidCrystal.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DHTPIN 10    // Connect the DHT sensor to digital pin 2
+#define DHTTYPE DHT11   // Change to DHT11 if you're using that sensor
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+DHT dht(DHTPIN, DHTTYPE);
+
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);  // Connect the LCD to pins 8, 9, 4, 5, 6, 7
+
+void setup() 
+  lcd.begin(16, 2);  // Initialize the LCD with 16 columns and 2 rows
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  delay(50);  // Delay between readings
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  float humidity = dht.readHumidity();
+  float temperature =dht.readTemperature();
+
+  lcd.clear();  // Clear the LCD display
+
+  lcd.setCursor(0, 0);  // Set the cursor to the first column, first row
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" C");
+
+
+  lcd.setCursor(0, 1);  // Set the cursor to the first column, second row
+  lcd.print("Humidity: ");
+  lcd.print(humidity);
+  lcd.print(" %");
 }
